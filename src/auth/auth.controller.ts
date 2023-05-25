9055c4662a7c660cad3c1f0c2e5f424ea58d891f
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2023-05-25 21:21:38 +0200

    Add logout

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Delete,
   Post,
   Request,
   Response,
@@ -8,6 +9,7 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dtos/register.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
 
 @Controller('auth')
@@ -24,6 +26,15 @@ export class AuthController {
   async login(@Request() req, @Response() res) {
     const tokens = await this.authService.createSession(req.user);
     res.cookie('auth', tokens, { httpOnly: true });
+    res.send({
+      message: 'success',
+    });
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('/logout')
+  async logout(@Response() res) {
+    res.clearCookie('auth', { httpOnly: true });
     res.send({
       message: 'success',
     });
